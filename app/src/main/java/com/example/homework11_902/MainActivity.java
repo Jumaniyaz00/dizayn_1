08c89dc7ec42fce92710e2427f74c6970011755d
Jumaniyaz00 <jumaniyoz00@mail.ru>
2020-03-10 09:59:44 +0600

    Initial commit

---
+++ app/src/main/java/com/example/homework11_902/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.homework11_902;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        textView = findViewById(R.id.arrivest);
+//        textView = findViewById(R.id.departs);
+
+    }
+}
